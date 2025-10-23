a1882cc69ba0c8c8e85ed67f9b4cceee6e0de4b8
zoealfordmoss-spec <zoealfordmoss@seattleacademy.org>
2025-10-23 16:27:30 +0000

    hiiii

--- Projects/code_art.py
+++ Projects/code_art.py
@@ -2,15 +2,17 @@ import turtle
 turtle.Screen().bgcolor("pink")
 t = turtle.Turtle()
 t.speed(9)
-# i made the speed a little bit less because they said it was to slow
+# i made the speed a little bit less because they said it was to fast
 
 
 t.goto(100, 0)
 colors =  ["red", "orange", "yellow", "green", "blue", "purple"]
+# i did the rainbow because it looks pretty against pink
 for i in range(350):
     t.color(colors [ i % 6])
     t.forward(90 + i)
     t.left(46)
 
 turtle.exitonclick()
+# i made it at the side becaue i think it looks cooler than in the middle
 
